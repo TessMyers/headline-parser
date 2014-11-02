fe941b62855a6a5ce954508d3b130cd20fc6de65
Tess Myers <tess@tessmyers.com>
2014-11-02 14:10:00 -0800

    (fix) parser returns max keywords, handles dumb errors

--- headline-parser.js
+++ headline-parser.js
@@ -1,21 +1,15 @@
 var extractor = require('keyword-extractor');
 
-module.exports = {
-  findKeywords: findKeywords
-}
-
 var findKeywords = function( headline, body, n, keywordArgs ){
-  n = n || 1;
   keywordArgs = keywordArgs || { language:"english", return_changed_case:true };
-
-  // Warn user if body or headline is not properly formatted
-  if (typeof body != 'string') { console.log('Whoops, you need to include a string for the body argument');};
-  if (typeof headline != 'string') { console.log('Whoops, you need to include a string for the headline argument');}
 
   body = body.split(' ');
 
   // Extract keywords from headline. Returns an array
   keywordArray = extractor.extract( headline, keywordArgs );
+
+  // if no N is given, set n to max length. // todo: just set to infinity?
+  n = n || keywordArray.length;
 
   // Create object to track each mention of keyword in summary
   keywordCount = {};
@@ -23,7 +17,7 @@ var findKeywords = function( headline, body, n, keywordArgs ){
 
   // Iterate over summary to count mentions of each keyword
   for ( var i = 0; i < body.length; i++ ) {
-    for (var word in keywordCount) {
+    for (word in keywordCount) {
       // console.log(word, body[i]);
       if (word === body[i]){
         keywordCount[word] += 1;
@@ -31,15 +25,23 @@ var findKeywords = function( headline, body, n, keywordArgs ){
     }
   }
 
+  // If no keywords have been mentioned, return an error
+  var aboveZero = Object.keys(keywordCount).filter(function(key){ return keywordCount[key] > 0; })
+
+  if (Object.keys(keywordCount).length > 1 && aboveZero.length < 1) {
+    var message = 'Sorry, this headline appears to be completely irrelevant to the article body. Here are your keywords anyway: ' + keywordArray.join(', ');
+    return message;
+  }
+
+
   // Sort keywords by highest number of mentions
   var sortedKeys = Object.keys(keywordCount).sort(function(a,b){return keywordCount[a] - keywordCount[b];});
 
   // Return last n important words, will return maximum if n is greater than max.
-  return sortedKeys.slice(-n)
-
-
+  return sortedKeys.slice(-n);
 }
 
-var x = findKeywords('stuff',' stuff stuff and', 1)
 
-console.log(x)
+module.exports = {
+  findKeywords: findKeywords
+}
