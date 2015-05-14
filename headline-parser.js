0c7dc7f706f7ea614c37d493ff212b40e1be18bf
Espen Klem <espen.klem@gmail.com>
2015-05-14 22:57:03 +0200

    Added proper checks on returnNonMatch Also added a new test to check both when returnNonMatch is true and false

--- headline-parser.js
+++ headline-parser.js
@@ -1,7 +1,9 @@
 var extractor = require('keyword-extractor');
 
-var findKeywords = function( headline, body, n, returnNonMatched, keywordArgs ){
+var findKeywords = function( headline, body, n, keywordArgs, returnNonMatched ){
   keywordArgs = keywordArgs || { language:"english", return_changed_case:true };
+  
+  // Set returnNonMatched to false if not given
   returnNonMatched = returnNonMatched || false;
 
   body = body.split(' ');
@@ -29,17 +31,20 @@ var findKeywords = function( headline, body, n, returnNonMatched, keywordArgs ){
   // If no keywords have been mentioned, and 'returnNonMatched == true' return non matched keywords from headline
   var aboveZero = Object.keys(keywordCount).filter(function(key){ return keywordCount[key] > 0; })
 
-  if (Object.keys(keywordCount).length > 1 && aboveZero.length < 1 && returnNonMatched) {
-    var nonMatchedKeys = keywordArray.join(', ');
-    return nonMatchedKeys;
+  if (Object.keys(keywordCount).length > 1 && aboveZero.length < 1 && !returnNonMatched) {
+    return [];
   }
+  else if (Object.keys(keywordCount).length > 1 && aboveZero.length < 1 && returnNonMatched) {
+    var nonMatchedKeys = keywordArray;
+    return nonMatchedKeys;
+  } 
+  else {
+    // Sort keywords by highest number of mentions
+    var sortedKeys = Object.keys(keywordCount).sort(function(a,b){return keywordCount[a] - keywordCount[b];});
 
-
-  // Sort keywords by highest number of mentions
-  var sortedKeys = Object.keys(keywordCount).sort(function(a,b){return keywordCount[a] - keywordCount[b];});
-
-  // Return last n important words, will return maximum if n is greater than max.
-  return sortedKeys.slice(-n);
+    // Return last n important words, will return maximum if n is greater than max.
+    return sortedKeys.slice(-n);
+  }
 }
 
 
